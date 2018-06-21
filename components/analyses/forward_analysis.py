f5bdb66040535f951a50f839ed4b3b09f41d54bf
freakyn00b <rajeshmjmdr02@gmail.com>
2018-06-21 20:57:21 +0530

    Added FunctionGraphVisitor class

--- components/analyses/forward_analysis.py
+++ components/analyses/forward_analysis.py
@@ -172,4 +172,64 @@ class GraphVisitor(object):
 
         self._reached_fixedpoint.add(node)
 
+class FunctionGraphVisitor(GraphVisitor):
+    def __init__(self, func, graph=None):
+        '''
+        :param knowledge.func func:
+        '''
+        super(FunctionGraphVisitor, self).__init__()
 
+        self.function = func
+
+        if graph is None:
+            self.graph = self.function.graph
+
+        else:
+            self.graph = graph
+
+    def startpoints(self):
+
+        return [self.function.startpoint]
+
+    def successors(self, node):
+        return list(self.graph.successors(node))
+
+    def predecessors(self, node):
+        return list(self.graph.predecessors(node))
+
+    def sort_nodes(self, nodes=None):
+        sorted_nodes = CFGUtils.quasi_topological_sort_nodes(self.graph)
+
+        if nodes is not None:
+            sorted_nodes = [n for n in sorted_nodes if n in set(nodes)]
+
+        return sorted_nodes
+
+    def startpoints(self):
+
+        # TODO: make sure all connected components are covered
+
+        start_nodes = [node for node in self.callgraph.nodes() if self.callgraph.in_degree(node) == 0]
+
+        if not start_nodes:
+            # randomly pick one
+            start_nodes = [self.callgraph.nodes()[0]]
+
+        return start_nodes
+
+    def successors(self, node):
+        return list(self.callgraph.successors(node))
+
+    def predecessors(self, node):
+
+        return list(self.callgraph.predecessors(node))
+
+    def sort_nodes(self, nodes=None):
+        sorted_nodes = CFGUtils.quasi_topological_sort_nodes(self.callgraph)
+
+        if nodes is not None:
+            sorted_nodes = [n for n in sorted_nodes if n in set(nodes)]
+
+        return sorted_nodes
+
+class SingleNodeGraphVisitor(GraphVisitor):
