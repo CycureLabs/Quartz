63da4c442a598c55c6049653601ec4d06ce5fc54
freakyn00b <rajeshmjmdr02@gmail.com>
2018-06-25 00:21:44 +0530

    Update

--- components/analyses/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-Code from angr framework for python
-https://github.com/angr/angr
-
-Modified some code for Quartz.
-'''
-
-from .cfg import Fast, Accurate, CFG, ArchOptions
--- components/misc/__init__.py
+++
@@ -1,3 +0,0 @@
-__author__ = 'Rajesh'
-
-from . import ux
